46cad23e473e80b11f4003f06e93fd23f17b3870
Alex Senik <alex.senik@samposoftware.com>
2017-08-29 12:13:36 +0300

    Update brakeman

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -1,4 +1,6 @@
 class CmsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def show
     @response = RestClient.get("#{ENV['BAZAAR_API_URL']}/cms/validate",
       params: { token: params[:token], access_token: params[:access_token] })
